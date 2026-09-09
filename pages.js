66f4623e7f79bbcbaba82c29323abe8acf553a29
shawn195 <shawn@wsc.school.nz>
2026-09-09 15:04:47 +1200

    added facts and pages to button

--- pages.js
+++ pages.js
@@ -3,4 +3,10 @@
 const pages = [
   "http:/eelslap.com",
   "https://memory.toys/classic/easy/",
+  "https://www.amazon.com/Raise-Your-Eating-Gifted-Children/dp/0595002366/",
+  "https://www.amazon.com.au/dp/045149492X/",
+  "https://musical.toys/toys/snapegiator/",
+  "https://pointerpointer.com/",
+  "https://cat-bounce.com/",
+
 ];
