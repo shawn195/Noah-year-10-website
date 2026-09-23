fc5eda1a20437b4e718b9423bceaa60cefa2ae72
shawn195 <shawn@wsc.school.nz>
2026-09-23 14:13:07 +1200

    added linkies

--- pages.js
+++ pages.js
@@ -1,8 +1,9 @@
 const pages = [
   "http://eelslap.com",
   "https://memory.toys",
-  "https://amazon.com",
-  "https://amazon.com.au",
+  "https://www.amazon.com.au/How-Talk-Your-About-Safety/dp/045149492X",
+  "https://www.amazon.com/dp/1613749414?lv=shuf&tag=judgeabook-20&channelId=1&plpRedirect=mhFallback",
+  "https://www.amazon.com/dp/0595002366?lv=shuf&tag=judgeabook-20&channelId=1&plpRedirect=mhFallback",
   "https://musical.toys",
   "https://pointerpointer.com",
   "https://cat-bounce.com",
